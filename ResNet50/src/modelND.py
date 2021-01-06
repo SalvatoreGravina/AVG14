833797ac67182bac411c749dc9e391fac95c6be5
salvatore <salvatore.gravina16@gmail.com>
2021-01-06 23:30:32 +0100

    Primo commit del progetto

---
+++ ResNet50/src/modelND.py
@@ -0,0 +1,42 @@
+import keras
+from . import resnet
+import keras.backend as K
+from keras.layers.experimental import preprocessing
+
+
+def Vggface2_ResNet50(input_dim=(224, 224, 3), nb_classes=101, mode='train', weight_decay = 5e-4):
+    # inputs are of size 224 x 224 x 3
+    data_augmentation = keras.Sequential(
+      [
+        preprocessing.RandomFlip("horizontal", name="random_horizontal_flip"),
+        preprocessing.RandomRotation(0.014, name="random_rotation"), #5gradi più o meno
+        #preprocessing.RandomZoom(0.1, name="random_zoom"),
+        #preprocessing.RandomCrop(200,200, name="random_crop"),
+        preprocessing.RandomContrast(0.5, name="random_contrast"),
+        preprocessing.Rescaling(1./255, name="rescaling")
+      ]
+    )
+    inputs = keras.layers.Input(shape=input_dim, name='base_input')
+    y = data_augmentation(inputs)
+    x = resnet.resnet50_backend(y)
+
+    # AvgPooling
+    x = keras.layers.AveragePooling2D((7, 7), name='avg_pool')(x)
+    x = keras.layers.Flatten()(x)
+    #x = keras.layers.Dense(512, activation='relu', name='dim_proj', kernel_regularizer=keras.regularizers.l2(weight_decay))(x) #aggiunto kernel_regularizer
+
+
+    if mode == 'train':
+        y = keras.layers.Dense(nb_classes, activation='softmax',
+                               use_bias=False, trainable=True,
+                               kernel_initializer='orthogonal',
+                               kernel_regularizer=keras.regularizers.l2(weight_decay),
+                               name='classifier_low_dim')(x)
+    else:
+        y = keras.layers.Lambda(lambda x: keras.backend.l2_normalize(x, 1))(x)
+
+    # Compile
+    model = keras.models.Model(inputs=inputs, outputs=y)
+    return model
+    
+
